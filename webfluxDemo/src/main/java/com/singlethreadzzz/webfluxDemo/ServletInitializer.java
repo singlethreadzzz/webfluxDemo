90f821d17574892a4a74946931c94b30825bc3ce
singlethreadzzz <singlethreadzzz@gmail.com>
2019-04-19 17:55:33 +0800

    工程初始化

--- webfluxDemo/src/main/java/com/singlethreadzzz/webfluxDemo/ServletInitializer.java
+++ webfluxDemo/src/main/java/com/singlethreadzzz/webfluxDemo/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.webfluxDemo;
+package com.singlethreadzzz.webfluxDemo;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
